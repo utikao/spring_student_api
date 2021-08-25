fc344997ba8cddffc1dbcb461d3ea0b492848ceb
utikao <67973670+utikao@users.noreply.github.com>
2021-08-25 15:45:51 +0300

    made new class StudentConfig that can add new students into database

--- src/main/java/com/example/spring/student/StudentConfig.java
+++ src/main/java/com/example/spring/student/StudentConfig.java
@@ -1,5 +1,41 @@
 package com.example.spring.student;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
 public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student james = new Student(
+
+                    "James",
+                    "James@gmail.com",
+                    LocalDate.of(2003, Month.MARCH,28),
+                    18
+
+            );
+
+            Student alex = new Student(
+
+                    "Alex",
+                    "Alex@gmail.com",
+                    LocalDate.of(2001, Month.MARCH,21),
+                    20
+
+            );
+
+            repository.saveAll(
+                    List.of(james,alex)
+            );
+
+        };
+    }
 }
