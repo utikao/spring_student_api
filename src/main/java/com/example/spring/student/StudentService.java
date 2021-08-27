ec494c4a11d99b62c431e898d3d43e7b679d73b5
utikao <67973670+utikao@users.noreply.github.com>
2021-08-27 19:06:37 +0300

    I have implemented POST functionality.Also I have implemented safety feature so user can not post student with the email already used int database.

--- src/main/java/com/example/spring/student/StudentService.java
+++ src/main/java/com/example/spring/student/StudentService.java
@@ -3,10 +3,14 @@ package com.example.spring.student;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class StudentService {
@@ -20,5 +24,14 @@ public class StudentService {
 
     public List<Student> getStudents(){
         return studentRepository.findAll();
+    }
+
+    public void addNewStudent(Student student) {
+        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+
+        if (studentOptional.isPresent()){
+            throw new IllegalStateException("email taken");
+        }
+        studentRepository.save(student);
     }
 }
